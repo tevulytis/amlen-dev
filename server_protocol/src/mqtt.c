e72161c66c44a0c2c684393dd58bc4f5acb53e5a
Emilis Venckus <emilis.venckus@de.ibm.com>
2025-01-28 08:32:59 +0100

    fix setting of outgoingQosFromConfig

--- server_protocol/src/mqtt.c
+++ server_protocol/src/mqtt.c
@@ -895,6 +895,7 @@ HOT int ism_mqtt_receive(ism_transport_t * transport, char * buf, int buflen, in
     mqttProtoObj_t * pobj = transport->pobj;
     int  mproplen;
     int  vilen;
+    uint8_t outgoingQosFromConfig;
 
     if (pobj == NULL) {
         ism_common_setError(ISMRC_Closed);
@@ -1001,7 +1002,7 @@ HOT int ism_mqtt_receive(ism_transport_t * transport, char * buf, int buflen, in
          */
         case MT_PUBLISH:
 
-            uint8_t outgoingQosFromConfig = (uint8_t)ism_security_context_getOutgoingQos(transport->security_context);
+            outgoingQosFromConfig = (uint8_t)ism_security_context_getOutgoingQos(transport->security_context);
             
             /*
              * Process a PUBLISHX which is only in proxy protocol
